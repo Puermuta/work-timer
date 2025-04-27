f5f1386c8504d761ab3bae2148a5ae897ffa7a9a
Puermuta <19885813+Puermuta@users.noreply.github.com>
2025-04-27 17:23:48 +0200

    Update TimeTable and WorkLogList styles; change button visibility and title text

--- src/components/TimeTable.jsx
+++ src/components/TimeTable.jsx
@@ -13,15 +13,21 @@ function TimeTable() {
     return (
             
         <div className = {style.container}>
-        <div className = {style.title}>{`Total hours`}</div>
+        <div className = {style.title}>{`Hours worked`}</div>
         <div style={{"height" : "50vh"}}>
         <WorkLogList workLogs ={workLogs} />
         </div>
-        {workLogs.length > 0 ? (
-            <button className = {style.btn} onClick = {toggleClearLogs}>Clear list</button>
-        ) : (
-            <div></div>
-        )}
+        <button 
+            onClick={toggleClearLogs} 
+            className={style.btn} 
+            style={{
+                opacity: workLogs.length > 0 ? 1 : 0,
+                visibility: workLogs.length > 0 ? 'visible' : 'hidden',
+                transition: 'opacity 0.5s, visibility 0.5s'
+            }}
+        >
+        Finish job
+        </button>
         </div>
         
     );
