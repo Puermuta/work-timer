0562e76466778920674183f7fa4ed1d2f52b4606
Puermuta <19885813+Puermuta@users.noreply.github.com>
2025-04-27 20:01:54 +0200

    Refactor styles in TimeTable and WorkLogList components; adjust margins, add listContainer class, and improve layout

--- src/components/TimeTable.jsx
+++ src/components/TimeTable.jsx
@@ -13,22 +13,22 @@ function TimeTable() {
     return (
             
         <div className = {style.container}>
-        <div className = {style.title}>{`Hours worked`}</div>
-        <div style={{"height" : "50vh"}}>
-        <WorkLogList workLogs ={workLogs} />
-        </div>
-        {/*<div className = {style.subText}>{`Total: ${(workLogs.reduce((acc, curr) => acc + curr.workedTime, 0)/360000).toFixed(2)} hours`}</div>*/}
-        <button 
-            onClick={toggleClearLogs} 
-            className={style.btn} 
-            style={{
-                opacity: workLogs.length > 0 ? 1 : 0,
-                visibility: workLogs.length > 0 ? 'visible' : 'hidden',
-                transition: 'opacity 0.5s, visibility 0.5s'
-            }}
-        >
-        Clear list
-        </button>
+            <div className = {style.title}>{`Hours worked`}</div>
+            <div style={{"height" : "50vh"}}>
+                <WorkLogList workLogs ={workLogs} className = {style.listContainer}/>
+            </div>
+            <div style={{"height" : "3vh"}}></div>
+            <button  
+                onClick={toggleClearLogs} 
+                className={style.btn} 
+                style={{
+                    opacity: workLogs.length > 0 ? 1 : 0,
+                    visibility: workLogs.length > 0 ? 'visible' : 'hidden',
+                    transition: 'opacity 0.5s, visibility 0.5s'
+                }}
+            >
+            Clear list
+            </button>
         </div>
         
     );
--- src/components/WorkLogList.jsx
+++ src/components/WorkLogList.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
-import style from "./WorkLogList.module.css";
 
-const WorkLogList = ({ workLogs }) => {
-  if (workLogs.length === 0) {
-    return <div>You have no logged work!</div>;
-  }
+const WorkLogList = ({ workLogs, className }) => {
+    if (workLogs.length === 0) {
+        return <div>You have no logged work!</div>;
+    }
 
     const convertToHours = (time) => {
         return (time / 3600000).toFixed(2) ;
     }
-  return (
-    <>
-      {workLogs.map((item) => (
-        <div key={item.time} className={style.listContainer}>
-          <div>{item.time}</div>
-          <div>{convertToHours(item.workedTime)}</div>
+
+    const totalHours = () => {
+        return (workLogs.reduce((acc, curr) => acc + curr.workedTime, 0)/3600000).toFixed(2);
+    }
+    return (
+    <>  
+        <div className = {className}>
+            <div>Date</div>
+            <div>Hours</div>
         </div>
-      ))}
+        <div style={{"display":"flex", "flexDirection":"column", "justifyContent":"space-between", "height":"43vh"}}>
+        {workLogs.map((item) => (
+            <div key={item.time} className = {className} style={{"backgroundColor": "#00000023"}}>
+            <div>{item.time}</div>
+            <div>{convertToHours(item.workedTime)}</div>
+            </div>
+        ))}
+            <div className = {className} style={{"backgroundColor": "#00000023"}}>
+            <div>Total:</div>
+            <div>{`${totalHours()}`}</div>
+            </div>
+        </div>
     </>
   );
 };
